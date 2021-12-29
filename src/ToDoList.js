3543bdecc02be68c2e3218e3677cfcad19a8fa1f
Alexmdz77 <alext77165@gmail.com>
2021-12-29 22:00:36 +0100

    fix Todolist & add EmailSenderService

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -12,7 +12,7 @@ export class ToDoList {
     }
 
     add(item){
-        if(this._checkAdd(item)){
+        if(this._checkAdd(item) == true){
             this.list.push(item);
             return 'Success !'
         } else {
@@ -26,31 +26,32 @@ export class ToDoList {
     _checkAdd(item) {
         // Check Nombre d'items
         const nbItems = this.getList().length;
-        
-        console.log(nbItems)
-        console.log(item)
 
-        if (nbItems >= 10 || nbItems < 0 ) {
+        if (nbItems >= 10 ) {
             return "Vous ne pouvez avoir que 10 items";
         }
-        if (nbItems == 8) {
+        else if (nbItems == 8) {
             EmailSenderService.send(this.user.email ,"Vous ne pouvez plus qu'ajouter 2 items");
+            return true;
         }
         
         // Check Nom de l'item
-        if(!item.name || this.getList().includes(item.name)){
+        if(!item.name || this.getList().filter(i => i.name == item.name).length > 0){
             return "Le nom de cet item n'est pas unique";
         }
 
         // Check Content de l'item
-        if(!item.content || item.content.split().length > 1000){
+        if(!item.content || item.content.length > 1000){
             return "Cet item contient trop de caractère";
         }
 
         // Check Date de création
-        if(!item.createdAt || DateTime.fromJSDate(this.getList().at(-1).createdAt).diffNow('minutes').minutes < - 30){
+        if(nbItems != 0 && (!item.createdAt || item.createdAt - this.getList().at(-1).createdAt < 1800000)){
             return "Vous avez déjà envoyé un item dans les 30 minutes";
+            //throw "Vous avez déjà envoyé un item dans les 30 minutes";
         }
+
+        return true;
 
     }
 
@@ -63,13 +64,6 @@ export class ToDoList {
     }
 
     getList(){
-        const items = [
-            {
-                name: 'Le nom',
-                content: 'Le contenu',
-                createdAt: 1639392693,
-            }
-        ]
-        return items;
+        return this.list;
     }
 }
--- tests/todolist.test.js
+++ tests/todolist.test.js
@@ -2,10 +2,11 @@ import { DateTime } from 'luxon';
 import { ApiEmailValidator } from '../src/ApiEmailValidator';
 import { User } from '../src/User';
 import { ToDoList } from '../src/ToDoList';
+import { EmailSenderService } from '../src/EmailSenderService';
 
 describe('test todolist functions', () => {
 
-    it('should be a good todolist validation', () => {
+    it('should be a good user validation', () => {
         const apiEmailValidator = new ApiEmailValidator()
         apiEmailValidator.check = jest.fn((email) => true)
         const user = new User({
@@ -13,6 +14,7 @@ describe('test todolist functions', () => {
             birthDate: DateTime.now().minus({ years: 20 }).toJSDate(),
             lastName: 'Trouvé',
             firstName: 'Alexandre',
+            password: 'password',
             apiEmailValidator
         })
         expect(
@@ -20,25 +22,158 @@ describe('test todolist functions', () => {
         ).toBe(true);
     });
 
-    it('should be a good user validation', () => {
+    it('should be a good todolist add validation', () => {
         const apiEmailValidator = new ApiEmailValidator()
         const user = new User({
             email: 'alext77165@gmail.com',
             birthDate: DateTime.now().minus({ years: 20 }).toJSDate(),
             lastName: 'Trouvé',
             firstName: 'Alexandre',
+            password: 'password',
             apiEmailValidator
         })
 
         const todolist = new ToDoList(user)
+        let now = Date.now()
+
         let item = {
             name: 'TEST du nom',
             content: 'TEST du contenu',
-            createdAt: Date.now(),
+            createdAt: now,
+        }
+        todolist.add(item);
+
+        expect(JSON.stringify(todolist.list)).toBe(JSON.stringify([item]));
+    });
+
+    it('should be a good todolist add two items validation', () => {
+        const apiEmailValidator = new ApiEmailValidator()
+        const user = new User({
+            email: 'alext77165@gmail.com',
+            birthDate: DateTime.now().minus({ years: 20 }).toJSDate(),
+            lastName: 'Trouvé',
+            firstName: 'Alexandre',
+            password: 'password',
+            apiEmailValidator
+        })
+
+        const todolist = new ToDoList(user)
+        let now = Date.now()
+        let datelimite = now + 1850000;
+        let item = {
+            name: '1er item',
+            content: 'contenu du 1er item',
+            createdAt: now,
+        }
+        todolist.add(item);
+        expect(JSON.stringify(todolist.list)).toBe(JSON.stringify([item]));
+
+        let item2 = {
+            name: '2ème item',
+            content: 'contenu du 2ème item',
+            createdAt: datelimite,
+        }
+        todolist.add(item2);
+        expect(JSON.stringify(todolist.list)).toBe(JSON.stringify([item, item2]));
+    });
+
+    it('should be a bad todolist add two items validation', () => {
+        const apiEmailValidator = new ApiEmailValidator()
+        const user = new User({
+            email: 'alext77165@gmail.com',
+            birthDate: DateTime.now().minus({ years: 20 }).toJSDate(),
+            lastName: 'Trouvé',
+            firstName: 'Alexandre',
+            password: 'password',
+            apiEmailValidator
+        })
+
+        const todolist = new ToDoList(user)
+        let now = Date.now()
+        let datelimite = Date.now();
+        let item = {
+            name: '1er item',
+            content: 'contenu du 1er item',
+            createdAt: now,
+        }
+        todolist.add(item);
+        expect(JSON.stringify(todolist.list)).toBe(JSON.stringify([item]));
+
+        let item2 = {
+            name: '2ème item',
+            content: 'contenu du 2ème item',
+            createdAt: datelimite,
+        }
+        expect(todolist.add(item2)).toBe("Vous avez déjà envoyé un item dans les 30 minutes");
+        expect(JSON.stringify(todolist.list)).toBe(JSON.stringify([item]));
+    });
+
+    it('should be a bad todolist add same items name validation', () => {
+        const apiEmailValidator = new ApiEmailValidator()
+        const user = new User({
+            email: 'alext77165@gmail.com',
+            birthDate: DateTime.now().minus({ years: 20 }).toJSDate(),
+            lastName: 'Trouvé',
+            firstName: 'Alexandre',
+            password: 'password',
+            apiEmailValidator
+        })
+
+        const todolist = new ToDoList(user, [{name: '1er item',content: 'contenu du 1er item',createdAt: Date.now()}])
+
+        let datelimite = Date.now() + 1850000;
+        let item2 = {
+            name: '1er item',
+            content: 'contenu du 2ème item',
+            createdAt: datelimite,
         }
 
-        console.log(todolist.add(item));
-        console.log(Date.now());
-        expect(todolist.add(item)).toBe(true);
+        expect(todolist.add(item2)).toBe("Le nom de cet item n'est pas unique");
+    });
+
+    it('should be a bad todolist too long item content validation', () => {
+        const apiEmailValidator = new ApiEmailValidator()
+        const user = new User({
+            email: 'alext77165@gmail.com',
+            birthDate: DateTime.now().minus({ years: 20 }).toJSDate(),
+            lastName: 'Trouvé',
+            firstName: 'Alexandre',
+            password: 'password',
+            apiEmailValidator
+        })
+
+        const todolist = new ToDoList(user)
+        
+        let item = {
+            name: '1er item',
+            content: 'contenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er itemcontenu du 1er item',
+            createdAt: Date.now(),
+        }
+        expect(todolist.add(item)).toBe("Cet item contient trop de caractère");
+    });
+    
+    it('should send a email if the list already containt 8 items', () => {
+        const apiEmailValidator = new ApiEmailValidator()
+        const user = new User({
+            email: 'alext77165@gmail.com',
+            birthDate: DateTime.now().minus({ years: 20 }).toJSDate(),
+            lastName: 'Trouvé',
+            firstName: 'Alexandre',
+            password: 'password',
+            apiEmailValidator
+        })
+        
+        const todolist = new ToDoList(user, [{name: 'test',content:'test',createdAt: Date.now()},{name: 'test',content:'test',createdAt: Date.now()},{name: 'test',content:'test',createdAt: Date.now()},{name: 'test',content:'test',createdAt: Date.now()},{name: 'test',content:'test',createdAt: Date.now()},{name: 'test',content:'test',createdAt: Date.now()},{name: 'test',content:'test',createdAt: Date.now()}])
+
+        let datelimite = Date.now() + 1850000;
+        let item = {
+            name: '1er item',
+            content: 'contenu du 1er item',
+            createdAt: datelimite,
+        }
+        todolist.add(item);
+
+        EmailSenderService.check = jest.fn((email) => true)
+        EmailSenderService.check(user.email, "Vous ne pouvez plus ajouter que deux items.")
     });
 })    
